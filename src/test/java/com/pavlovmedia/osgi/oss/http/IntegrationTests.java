ebc4e3908748fbcf31c5ec1c3f6d00b9bcadcc43
Ira Greenstein <igreenstein@pavlovmedia.com>
2018-06-01 11:11:28 -0500

    char-at-a-time-read -- Freshened up the sample groovy script

--- src/test/java/com/pavlovmedia/osgi/oss/http/IntegrationTests.java
+++ src/test/java/com/pavlovmedia/osgi/oss/http/IntegrationTests.java
@@ -16,39 +16,39 @@ import com.pavlovmedia.oss.osgi.http.PavlovHttpClientImpl;
 import com.pavlovmedia.oss.osgi.http.Spider;
 
 /**
- * 
+ *
  * @author Shawn Dempsay {@literal <sdempsay@pavlovmedia.com>}
  *
  */
 public class IntegrationTests {
     @Test
     public void testPavlovgo() throws Exception {
-        Optional<HttpResponse> response = new PavlovHttpClientImpl()
+        final Optional<HttpResponse> response = new PavlovHttpClientImpl()
                 .againstUrl(new URL("http://www.pavlovgo.com"))
                 .withVerb(HttpVerbs.GET)
                 .usingGzip()
                 .execute(System.err::println);
-        
+
         assertTrue(response.isPresent());
         System.out.println(response.get().responseHeaders);
         assertEquals(200, response.get().responseCode);
         assertTrue(response.get().responseStream.isPresent());
         System.out.println(response.get().getResponseText(System.err::println));
     }
-    
+
     @Test
     public void testSpider() throws Exception {
-        PavlovHttpClientImpl client = new PavlovHttpClientImpl();
-        HashMap<URL, HttpResponse> accumulator = new HashMap<>(); 
-        Spider spider = new Spider(client);
-        spider.doSpider(new URL("https://repo1.maven.org/maven2/com/pavlovmedia/oss/osgi/"), 
-                this::filterWalker, 
-                u -> u.toExternalForm().endsWith("maven-metadata.xml"), accumulator, 
+        final PavlovHttpClientImpl client = new PavlovHttpClientImpl();
+        final HashMap<URL, HttpResponse> accumulator = new HashMap<>();
+        final Spider spider = new Spider(client);
+        spider.doSpider(new URL("https://repo1.maven.org/maven2/com/pavlovmedia/oss/osgi/"),
+                this::filterWalker,
+                u -> u.toExternalForm().endsWith("maven-metadata.xml"), accumulator,
                 System.err::println);
         accumulator.forEach((u,v) -> System.out.println(String.format("%s -> \n%s\n\n", u, v.getResponseText())));
         assertEquals(5, accumulator.size());
     }
-    
+
     private boolean filterWalker(final URL url) {
         return !Arrays.asList(".pom", ".asc", ".jar", ".md5", "sha1").stream()
             .anyMatch(e -> url.toExternalForm().endsWith(e));

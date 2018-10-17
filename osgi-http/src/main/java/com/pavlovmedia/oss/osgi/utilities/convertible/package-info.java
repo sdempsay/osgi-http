d822fd9037412638eb3b5fd72bb4c20564faf9cf
Bryce Dillman <dillman19199@gmail.com>
2018-10-17 14:09:56 -0500

    fixing files

--- osgi-http/src/main/java/com/pavlovmedia/oss/osgi/utilities/convertible/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @author Shawn Dempsay {@literal <sdempsay@pavlovmedia.com>}
- * Part of a larger API set, included to get convertible assests
- */
-package com.pavlovmedia.oss.osgi.utilities.convertible;
